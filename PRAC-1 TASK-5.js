d083a4dd06b4e8ae94846e6570a295c9589437d5
Brijesh Patel <97022007+21it107@users.noreply.github.com>
2023-07-10 22:57:16 +0530

    Add files via upload

---
+++ PRAC-1 TASK-5.js
@@ -0,0 +1,8 @@
+// Array of favorite books
+const favoriteBooks = ['Maths','Physics','Chemistry'];
+    function displayBooks(books) {
+    for (let i = 0; i < books.length; i++) {
+        console.log(books[i]);
+    }
+}
+displayBooks(favoriteBooks);
